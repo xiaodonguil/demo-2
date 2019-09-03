208acd3e51e2e23580b5fc72e705869073f69cdc
unknown <liuxiaodong@ict.ac.cn>
2019-09-03 10:32:23 +0800

    添加AND

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo2Application {
-    System.out.println("main 入口");
+    
     public static void main(String[] args) {
         SpringApplication.run(Demo2Application.class, args);
     }
-    
+
 }
